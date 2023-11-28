59d14f7fe8c8c351bc2d3115959d0cd55754b0ec
Swopnil Parajuli <swopnilparajuli01@gmail.com>
2023-11-28 19:29:53 +0545

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <TodoWrapper/>
+      <div className="Footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import { Footer } from 'flowbite-react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className = "Footer_component">
+        © 2023 Swopnil Parajuli. All rights reserved.
+    </footer>
+  )
+}
