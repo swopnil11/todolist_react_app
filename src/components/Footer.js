71530a0b656adb53df203659621461f17183b2f6
Swopnil Parajuli <swopnilparajuli01@gmail.com>
2023-12-14 23:24:07 +0545

    Footer Changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className = "Footer_component">
-        © 2023 Swopnil Parajuli. All rights reserved.
+      © 2023 Swopnil Parajuli. All rights reserved.&nbsp;
+      <a href="https://github.com/swopnil11"><i className="fab fa-github"></i></a>&nbsp;
+      <a href="https://linkedin.com/in/swopnil-parajuli"><i class="fab fa-linkedin"></i></a>
     </footer>
   )
 }
